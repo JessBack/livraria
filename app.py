8d129d718466911a95008d3cdcb03d77c044132b
JessBack <jessica.filipe@jstechnology.co.mz>
2024-11-28 09:31:07 +0200

    Criacao de tabelas na base de dados

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from config import Config  # Importa a classe de configuração
-from models import models  # Importa a classe de configuração
 from flask_cors import CORS
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from datetime import datetime
 
-
+# Inicializando a aplicação Flask
 app = Flask(__name__)
 
 # Configuração do banco de dados usando a classe Config
@@ -17,13 +17,31 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 CORS(app)
 
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    available = db.Column(db.Boolean, default=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+class Loan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    loan_date = db.Column(db.DateTime, default=datetime.utcnow)
+    return_date = db.Column(db.DateTime, nullable=True)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
 
-# Criação das tabelas e execução da aplicação
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()  # Criar todas as tabelas no banco de dados
+        db.create_all()  
+
     app.run(debug=True)
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ class Config:
     USER = "root"
     PASSWORD = ""
     HOST = "localhost"
-    DATABASE = "livraria"
+    DATABASE = "library"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False  
     SECRET_KEY = "minha_chave_1234"  # Corrigido o nome para SECRET_KEY
